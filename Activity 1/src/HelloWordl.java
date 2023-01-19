6f70f5ac67aef1a7b850ee4d46d414d80fcb6348
Logan Cardoso <102470246+Logan-cic@users.noreply.github.com>
2023-01-18 23:15:54 -0300

    Hello, world in Java

---
+++ Activity 1/src/HelloWordl.java
@@ -0,0 +1,15 @@
+public class HelloWordl {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+// public: So that java virtual machine(JVM) can execute the method from anywhere.
+
+// static: The main method is to be called without an object. The modifiers public and static can be written in either order.
+
+// void: The main method doesn’t return anything.
+
+// main(): Name configured in the JVM. The main method must be inside the class definition. The compiler executes the codes starting always from the main   function.
+
+// String[]: The main method accepts a single argument, i.e., an array of elements of type String.
